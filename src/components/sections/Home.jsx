f14bcf51e64c72cc4881770e7928cebfce8e7008
janaahmedelsayed <168267766+janaahmedelsayed@users.noreply.github.com>
2025-04-25 01:01:17 +0300

    Fix image path

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -117,7 +117,7 @@ Css. Strong understanding of frontend architecture, component-based design, and 
           <div className="flex-1 flex justify-center items-center">
             <div className="w-60 h-60 sm:w-72 sm:h-72 md:w-80 md:h-80 rounded-full overflow-hidden shadow-xl transition-transform duration-500 hover:scale-125 mt-8">
               <img
-                src="https://github.com/janaahmedelsayed/Myportfolio/blob/main/public/assets/jana.jpg"
+               src="./assets/jana.jpg"
                 alt="Jana Ahmed"
                 className="w-full h-full object-cover"
               />
